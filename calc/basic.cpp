171c4bb223c2529dc0554182cc59361d6d2a2e5d
202207066 <hansol5818@naver.com>
2025-06-13 16:31:57 +0900

    dev/a에서 commit함

--- calc/basic.cpp
+++ calc/basic.cpp
@@ -3,7 +3,11 @@ int add(int __x, int  __y)
 	return __x + __y;
 }
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> qq11
+>>>>>>> a112
 int sub(int __x, int  __y)
 {
 	return __x - __y;
--- calc/mul.h
+++ calc/mul.h
@@ -1,2 +1,6 @@
 #pragma once
+///
+///
+///12213132151
+///
 int mul(int __x, int __z);
